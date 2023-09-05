bd0f7de86aeb7811d0b91d4d549821773116569a
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-09-05 16:14:02 -0300

    feat: deixar home page dinâmico

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Article } from 'src/app/interfaces/article';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  others: Article[];
+  main: Article;
+
+  constructor(private articleService: ArticleService) {
+    this.others = articleService.fetchOthers();
+    this.main = articleService.fetchFirst();
+  }
 
 }
--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -6,22 +6,32 @@ import { articlesFaker } from '../data/dataFaker';
   providedIn: 'root',
 })
 export class ArticleService {
-  // constructor() {}
-
-  fetchArticles(): Article[] {
-    return articlesFaker;
-  }
+  constructor() {}
 
   findArticleById(id: string): Article {
     const article: Article | undefined = articlesFaker.find(
       (article) => article.id === id
     );
-    
+
     // Verifica se article não é undefined.
     if (article) {
       return article;
     }
 
     throw new Error(`Article not found for id: ${id}`);
+  }
+
+  fetchFirst(): Article {
+    return articlesFaker[0];
+  }
+
+  fetchOthers(): Article[] {
+    const others: Article[] = [];
+
+    for (let i = 1; i < articlesFaker.length; i++) {
+      others.push(articlesFaker[i]);
+    }
+
+    return others;
   }
 }
