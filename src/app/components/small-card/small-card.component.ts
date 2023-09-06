89031eaf9122cd38d97e94fb97922a181c638b15
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-09-06 15:08:11 -0300

    feat: responsividade para telas com max-width: 320px

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -4,7 +4,7 @@ import { Article } from 'src/app/interfaces/article';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css', './small-card.responsive.component.css']
 })
 export class SmallCardComponent {
   @Input({required: true}) article?: Article;
